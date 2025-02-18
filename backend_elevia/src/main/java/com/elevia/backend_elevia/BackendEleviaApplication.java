41d6e313963c7135da4932a07d12043f893577a3
JaviSGalindo <javier2006galindo@gmail.com>
2025-02-18 16:31:31 -0500

    descarga spring

---
+++ backend_elevia/src/main/java/com/elevia/backend_elevia/BackendEleviaApplication.java
@@ -0,0 +1,13 @@
+package com.elevia.backend_elevia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEleviaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendEleviaApplication.class, args);
+	}
+
+}
---
+++ backend_elevia/src/test/java/com/elevia/backend_elevia/BackendEleviaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elevia.backend_elevia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendEleviaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
