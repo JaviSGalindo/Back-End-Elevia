5f69b84fdb138f3e396034673f155d1fd64165d6
Luciatineo09 <amaliatineo09@hotmail.com>
2025-02-19 10:40:29 -0500

    CarritoService

--- backend_elevia/src/main/java/com/elevia/backend_elevia/service/CarritoService.java
+++ backend_elevia/src/main/java/com/elevia/backend_elevia/service/CarritoService.java
@@ -1,4 +1,56 @@
 package com.elevia.backend_elevia.service;
 
-public class CarritoService {
+import com.elevia.backend_elevia.model.Carrito;
+import com.elevia.backend_elevia.model.VarianteProducto;
+import com.elevia.backend_elevia.repository.IcarritoRepository;
+import com.elevia.backend_elevia.repository.IvarianteProductoRepository;
+import com.elevia.backend_elevia.validator.GenericValidator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CarritoService implements IcarritoService{
+
+    //Se instancia un validador genérico
+    private final GenericValidator genericValidator;
+    private final IcarritoRepository carritoRepository;
+
+
+    @Autowired
+    public CarritoService(GenericValidator genericValidator, IcarritoRepository carritoRepository) {
+        this.genericValidator = genericValidator; //Se agrega al constructor
+        this.carritoRepository = carritoRepository;
+    }
+
+    @Override
+    public Carrito getCarritoById(Long id) {
+        genericValidator.validateId(id, "Carrito");
+
+        Carrito carrito = carritoRepository.findById(id).orElse(null);
+        genericValidator.validateExists(carrito, "Carrito");
+        return carrito;
+    }
+
+    @Override
+    public Carrito createCarrito(Carrito carrito) {
+        genericValidator.validateExists(carrito, "Carrito");
+        return carritoRepository.save(carrito);
+    }
+
+    @Override
+    public Carrito updateCarrito(Long id, Carrito carrito) {
+        genericValidator.validateId(id, "Carrito");
+        genericValidator.validateExists(carrito, "Carrito");
+
+        carrito.setCantidad(carrito.getCantidad());
+        return carritoRepository.save(carrito);
+    }
+
+    @Override
+    public void deleteCarrito(Long id) {
+        genericValidator.validateId(id, "Carrito");
+        genericValidator.validateExists(getCarritoById(id), "Carrito");
+
+        carritoRepository.deleteById(id);
+    }
 }
--- backend_elevia/src/main/java/com/elevia/backend_elevia/service/IcarritoService.java
+++ backend_elevia/src/main/java/com/elevia/backend_elevia/service/IcarritoService.java
@@ -1,4 +1,13 @@
 package com.elevia.backend_elevia.service;
 
+import com.elevia.backend_elevia.model.Carrito;
+
 public interface IcarritoService {
+    Carrito getCarritoById(Long id);
+
+    Carrito createCarrito(Carrito carrito);
+
+    Carrito updateCarrito(Long id, Carrito carrito);
+
+    void deleteCarrito(Long id);
 }
