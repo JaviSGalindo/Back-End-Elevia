68b339ecff3b9aa3c7c61a250c51567e5bee57f2
JaviSGalindo <javier2006galindo@gmail.com>
2025-02-20 16:37:07 -0500

    usuarios

--- backend_elevia/src/main/java/com/elevia/backend_elevia/controller/UsuarioController.java
+++ backend_elevia/src/main/java/com/elevia/backend_elevia/controller/UsuarioController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/usuario")
+@RequestMapping("/usuarios")
 public class UsuarioController {
 
     private final IUsuarioService usuarioService;
