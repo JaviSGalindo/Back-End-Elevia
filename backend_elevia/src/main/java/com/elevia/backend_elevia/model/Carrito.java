77dcc44a54def43bb5f0be29cbb7dbfe6d633f01
Luciatineo09 <amaliatineo09@hotmail.com>
2025-02-20 17:06:42 -0500

    pequeños cambios

--- backend_elevia/src/main/java/com/elevia/backend_elevia/model/Carrito.java
+++ backend_elevia/src/main/java/com/elevia/backend_elevia/model/Carrito.java
@@ -14,17 +14,16 @@ public class Carrito {
     private Long id_carrito;
     private Long usuario_id;
     private Long variante_producto_id;
-    private Integer cantidad;
+    private int cantidad;
 
     public Carrito() {
     }
 
-    public Carrito(Long id_carrito, Long usuario_id, Long variante_producto_id, Integer cantidad) {
+    public Carrito(Long id_carrito, Long usuario_id, Long variante_producto_id, int cantidad) {
         this.id_carrito = id_carrito;
         this.usuario_id = usuario_id;
         this.variante_producto_id = variante_producto_id;
         this.cantidad = cantidad;
     }
-
 }
 
