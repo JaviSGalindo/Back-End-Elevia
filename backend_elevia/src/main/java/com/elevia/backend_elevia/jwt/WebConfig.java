04a22e64950a35ffe3025c9e9d7261f5e4e3ebef
JaviSGalindo <javier2006galindo@gmail.com>
2025-02-25 10:07:54 -0500

    ajustes en server

--- backend_elevia/src/main/java/com/elevia/backend_elevia/jwt/WebConfig.java
+++ backend_elevia/src/main/java/com/elevia/backend_elevia/jwt/WebConfig.java
@@ -12,9 +12,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "http://localhost:5500", "http://localhost:63342")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("Authorization", "Content-Type")
+                .allowedOrigins("http://127.0.0.1:5500", "http://localhost:4200", "http://localhost:5500", "http://localhost:63342")
+                .allowedMethods("*")
+                .allowedHeaders("*")
                 .allowCredentials(true);
     }
 }
